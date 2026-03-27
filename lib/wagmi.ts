f71ae1f9d65c3487d891ac653fc9cc29d1a0c56e
CaesarDickens <CaesarDickens@users.noreply.github.com>
2026-03-27 16:16:47 +0800

    Set live Builder Code attribution

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -1,4 +1,4 @@
 import { Attribution } from "ox/erc8021";
 
-export const BUILDER_CODE = "BUILDER_CODE_PLACEHOLDER";
+export const BUILDER_CODE = "bc_kzahy2fn";
 export const DATA_SUFFIX = Attribution.toDataSuffix({ codes: [BUILDER_CODE] });
